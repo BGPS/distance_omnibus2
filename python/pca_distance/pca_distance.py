eddcecd011a240877e50dca6d5e523a09756166f
Erik Rosolowsky <rosolowsky aatt gmail.com>
2013-08-14 13:15:22 -0700

    Fill in subcube extraction.  Still need to turn this into a PDF.

--- python/pca_distance/pca_distance.py
+++ python/pca_distance/pca_distance.py
@@ -2,7 +2,9 @@ import numpy as np
 from astropy.io import fits
 import astropy.nddata as nd
 import matplotlib.pyplot as p
-
+import scipy.ndimage as ndim
+import astropy.wcs as wcs
+import scipy.stats as stats
 def pca_distance(data, ChannelWidth = 1.0, PixelScale = 1.0, plot = False):
     nv = data.shape[0]
     nspatial = data.shape[1]*data.shape[2]
@@ -62,8 +64,12 @@ def pca_distance(data, ChannelWidth = 1.0, PixelScale = 1.0, plot = False):
 #        Dv[index] = VBins[np.min(np.where(BinnedVMeans<np.exp(-1)))]
 
 
-    UseIndex = np.where((Dv > 1)*(Dr > 1))    
-    Distance = ((Dv*ChannelWidth)/(0.87))**(1/0.65)*(Dr*PixelScale*np.pi/180)**(-1.0)
+    UseIndex = np.where((Dv > 2)*(Dr > 2))   
+    # Plslope,logIntercept,rval,pval,stderr = \
+    #     stats.linregress(np.log(Dr[UseIndex]),np.log(Dv[UseIndex]))
+    # Intercept = np.exp(logIntercept)
+    Distance = ((Dv*ChannelWidth)/(0.87))**(1/0.67)\
+        *(Dr*PixelScale*np.pi/180)**(-1.0)
     Distance = np.exp(np.mean(np.log(Distance[UseIndex])))
     if plot:
         p.loglog((Dr[UseIndex]*PixelScale*np.pi/180*Distance),\
@@ -72,3 +78,29 @@ def pca_distance(data, ChannelWidth = 1.0, PixelScale = 1.0, plot = False):
         p.ylabel('$\delta v$ (km/s)')
         p.show()
     return(Distance)
+
+def subcube_extract(glon, glat, vlsr, \
+                        GRSfile='/UBC-O/erosolo/astro/bgps/scutum/edmonton2013/grs-30-cube.fits', \
+                        border = [30,50,50], threshold = 0.4):
+    GRSCube = fits.getdata(GRSfile)
+    GRSheader = fits.getheader(GRSfile)
+    w = wcs.WCS(GRSheader)
+    pixels = w.wcs_world2pix([[glon,glat,vlsr]],1)
+# Make consistent with pixel ordering
+    pixels = (pixels[0,::-1])
+
+    x1 = int( np.floor( max([pixels[2]-border[2],0]) ) )
+    x2 = int( np.ceil( min([pixels[2]+border[2],GRSCube.shape[2]]) ) )
+    y1 = int( np.floor( max([pixels[1]-border[1],0]) ) )
+    y2 = int( np.ceil( min([pixels[1]+border[1],GRSCube.shape[1]]) ) )
+    v1 = int( np.floor( max([pixels[0]-border[0],0]) ) )
+    v2 = int( np.ceil( min([pixels[0]+border[0],GRSCube.shape[0]]) ) )
+
+    subcube = GRSCube[v1:v2,y1:y2,x1:x2]
+    label,nFeature = ndim.label(subcube>threshold)
+    hit = np.asarray(pixels-[v1,y1,x1],dtype='int8')
+
+    mask = (label==label[hit[0],hit[1],hit[2]])
+    
+    subcube[np.logical_not(mask)]=np.nan
+    return(subcube)
